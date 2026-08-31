a06bc5fe67eef9030084ea063eb8ed8116ad5282
nbroshar <nbroshar@dunwoody.edu>
2026-08-31 07:22:19 -0500

    removed old files

--- Week 2/old/instructor_solution/pcnt_test.s
+++
@@ -1,9 +0,0 @@
-# Reference test for the custom-0 popcount instruction.
-# pcnt rd, rs1  ->  rd = number of set bits in rs1.
-    li   x1, 255      # 0xFF, popcount = 8
-    pcnt x2, x1       # x2 = 8
-    add  x4, x2, x2   # exercise a normal op too (x4 = 16)
-    li   x3, 256
-    sw   x2, 0(x3)    # write result (8) to the output address
-done:
-    j    done
