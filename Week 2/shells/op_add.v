70b1a71553255924157a34ac192279d720d22b8c
nbroshar <nbroshar@dunwoody.edu>
2026-09-03 09:21:27 -0500

    lab2

---
+++ Week 2/shells/op_add.v
@@ -0,0 +1,6 @@
+`timescale 1ns/1ps
+// op_add -- 4-bit adder block.  STRUCTURE PHASE: wire it empty in IP Integrator.
+// BEHAVIOR PHASE: add the TODO line.  (4-bit add; a carry out of bit 3 wraps.)
+module op_add(input [3:0] a, input [3:0] b, output [3:0] y);
+    // TODO (behavior):  assign y = a + b;
+endmodule
